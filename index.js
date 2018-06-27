ba7614e60c8a06fe738d025b366c08e0fb3e7f38
JarekSopinski <jarek.sopinski@gmail.com>
2018-06-27 22:46:49 +0200

    Middleware & handling POST requests

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const port = 4000;
 
 // set up express app
 const app = express();
+
+app.use(bodyParser.json());
 
 // initialize routes specified in api.js file
 app.use('/api', require('./routes/api'));
