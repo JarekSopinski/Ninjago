53f076171ed4fabab0bbacca8ed2363704f1800e
JarekSopinski <jarek.sopinski@gmail.com>
2018-06-27 21:53:27 +0200

    Creating routes

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 4000;
 // set up express app
 const app = express();
 
+// initialize routes specified in api.js file
+app.use('/api', require('./routes/api'));
+
 // listen for requests
 app.listen(process.env.port || port, () => {
     console.log('Now listening for requests');
