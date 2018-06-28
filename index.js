e98675cad1a11b40a100a64e3340728c310d4127
JarekSopinski <jarek.sopinski@gmail.com>
2018-06-28 07:00:24 +0200

    Error handling

--- index.js
+++ index.js
@@ -11,10 +11,22 @@ const app = express();
 mongoose.connect('mongodb://localhost/ninjago');
 mongoose.Promise = global.Promise;
 
-app.use(bodyParser.json()); // first middleware
+// MIDDLEWARE STACK
 
-// initialize routes specified in api.js file
+// 1st middleware - parse data
+app.use(bodyParser.json());
+
+// 2nd middleware - initialize routes specified in api.js file
 app.use('/api', require('./routes/api'));
+
+// 3rd middleware - error handling (custom middleware)
+app.use((err,req,res,next) => {
+    //console.log(err)
+    // change status, then return error msg:
+    res.status(422).send({error: err.message});
+})
+
+// END OF MIDDLEWARE STACK
 
 // listen for requests
 app.listen(process.env.port || port, () => {
--- routes/api.js
+++ routes/api.js
@@ -3,26 +3,26 @@ const router = express.Router();
 const Ninja = require('../models/ninja');
 
 // get a list of ninjas from the db
-router.get('/ninjas', (req, res) => {
+router.get('/ninjas', (req, res, next) => {
     res.send({type:'GET'})
 });
 
 // add a new ninja to the db
-router.post('/ninjas', (req, res) => {
+router.post('/ninjas', (req, res, next) => {
     // create a new instance of Ninja Model from ninja.js:
     Ninja.create(req.body).then(ninja => {
         // once Promise fullfilled, send data to db:
         res.send(ninja)
-    });
+    }).catch(next) // in case of error go to the next middleware
 });
 
 // update a ninja in the db
-router.put('/ninjas/:id', (req, res) => {
+router.put('/ninjas/:id', (req, res, next) => {
     res.send({type:'PUT'})
 });
 
 // delete a ninja from the db
-router.delete('/ninjas/:id', (req, res) => {
+router.delete('/ninjas/:id', (req, res, next) => {
     res.send({type:'DELETE'})
 });
 
